673410d3e948e178075c15415e8de687360392b3
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-11-12 16:00:38 -0300

    Movimentação alterada, funções para herdar adicionadas

--- Castelo Sobre Rodas/Assets/BasicElements/Scripts/Player.cs
+++ Castelo Sobre Rodas/Assets/BasicElements/Scripts/Player.cs
@@ -4,69 +4,62 @@ using UnityEngine.InputSystem;
 public class PlayableCharacter : MonoBehaviour
 {
 
-    private Rigidbody2D rigidBody;
-    private Animator animator;
-    public InputActionReference movementInput;
-	public InputActionReference primaryInput;
-	public InputActionReference secondaryInput;
+	private Rigidbody2D rigidBody;
+	private Animator animator;
 
-    public float moveSpeed;
+	public float moveSpeed;
 
-    private Vector2 moveDirection;
+	private Vector2 moveDirection;
 
-
-
+	// Puxa os componentes para uso no código
 	private void Start()
 	{
 		rigidBody = GetComponent<Rigidbody2D>();
 		animator = GetComponent<Animator>();
 	}
 
-    private void OnEnable()
-    {
-        movementInput.action.Enable(); 
-        primaryInput.action.Enable(); 
-        secondaryInput.action.Enable(); 
-    }
-    private void OnDisable()
-    {
-        movementInput.action.Disable();
-        primaryInput.action.Disable(); 
-        secondaryInput.action.Disable(); 
-    }
+	// Retorna direção cardinal do personagem
+	private int GetCardinalDirection(Vector2 direction)
+	{
+
+		if (direction == Vector2.zero) return 0;
+
+		if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+		{
+			if (direction.x > 0) return 1; else return 3;
+		}
+		else
+		{
+			if (direction.y > 0) return 2; else return 4;
+		}
+
+	}
+
+	// Função puxada somente quando houver alteração no input
+	public virtual void ApplyMovement(InputAction.CallbackContext context)
+	{
+		moveDirection = context.ReadValue<Vector2>();
+
+		// Troca a direção na qual o personagem está andando na animação
+		animator.SetInteger("Direction", GetCardinalDirection(moveDirection));
+	}
+
+	// Aplica movimentação quando o frame não for pulado
+	public virtual void FixedUpdate()
+	{
+		rigidBody.linearVelocity = moveDirection * moveSpeed;
+	}
+
+	// Para uso nas classes que herdarem dessa
+	public virtual void LeftClickAction(InputAction.CallbackContext context)
+	{
+
+	}
 
 
-    private int GetCardinalDirection(Vector2 direction)
-    {
-        // Define direção do personagem na animação
-        
-        if (direction == Vector2.zero) return 0;
-        
+	public virtual void RightClickAction(InputAction.CallbackContext context)
+	{
 
-        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-        {
-            if (direction.x > 0) return 1; else return 3;
-        }
-        else
-        {
-            if (direction.y > 0) return 2; else return 4;
-        }
-    }
+	}
 
-    public virtual void Update()
-    {
-        // Altera moveDirection todos os frames
-        moveDirection = movementInput.action.ReadValue<Vector2>();
-
-        // Faz a animação usando moveDirection todos os frames
-        int directionCode = GetCardinalDirection(moveDirection);
-        animator.SetInteger("Direction", directionCode);
-    }
-
-
-    public virtual void FixedUpdate()
-    {
-		// Aplica movimentação quando o frame não for pulado
-        rigidBody.linearVelocity = moveDirection * moveSpeed;
-    }
 }
